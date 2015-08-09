bb293cfcd6a5d86101510af0072a64e7fcbc4ca8
brianjenkins100 <brianjenkins@mykolab.com>
2015-08-09 14:29:18 -0400

    First Commit

---
+++ build/bundle/programs/server/packages/ewall_foundation-4.js
@@ -0,0 +1,14 @@
+(function () {
+
+/* Imports */
+var Meteor = Package.meteor.Meteor;
+
+
+
+/* Exports */
+if (typeof Package === 'undefined') Package = {};
+Package['ewall:foundation-4'] = {};
+
+})();
+
+//# sourceMappingURL=ewall_foundation-4.js.map
